8c2823971d0a492cc72d371363289ce9234ca45e
Perkles <luiz.antonio.da.silva777@gmail.com>
2022-01-30 10:17:26 -0300

    Adicionar arquivos de projeto.

---
+++ dio-dotnet-poo-lab-2-master/Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace DIO.Series
+{
+    public abstract class EntidadeBase  //class abstrata base e todas as classes vão usar seu ID
+    {
+        public int Id { get; protected set; }
+
+    }
+}
